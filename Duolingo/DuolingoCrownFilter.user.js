027b2968cec7b8ba74d29ee081ace2755e6a00da
Milan Košťák <milankostak@seznam.cz>
2018-04-07 20:17:12 -0500

    Duolingo Crown Filter

---
+++ Duolingo/DuolingoCrownFilter.user.js
@@ -0,0 +1,64 @@
+// ==UserScript==
+// @name         Duolingo Crown Filter
+// @namespace    https://github.com/milankostak/Websites-tweaks/
+// @version      1.0.0
+// @description  Filter skills by crown count
+// @author       Milan Košťák
+// @match        https://www.duolingo.com/
+// @grant        none
+// @updateURL    https://github.com/milankostak/Websites-tweaks/raw/master/Duolingo/DuolingoCrownFilter.user.js
+// @downloadURL  https://github.com/milankostak/Websites-tweaks/raw/master/Duolingo/DuolingoCrownFilter.user.js
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    let nodes = document.querySelectorAll(".kOubg");
+
+    function show(i) {
+        nodes[i].parentNode.parentNode.parentNode.parentNode.parentNode.style.display = "inline-block";
+    }
+
+    function showAll() {
+        for (let i = 0; i < nodes.length; i++) {
+            show(i);
+        }
+    }
+
+    function hide(i) {
+        nodes[i].parentNode.parentNode.parentNode.parentNode.parentNode.style.display = "none";
+    }
+
+    for (let i = 0; i <= 5; i++) {
+        let a = document.createElement("a");
+        a.href = "#";
+        a.style.padding = "8px 20px";
+        a.style.border = "2px solid #dadada";
+        a.style.position = "relative";
+        a.style.top = "15px";
+        a.style.borderRadius = "50px";
+        a.style.color = "#999";
+        a.style.margin = "12px 12px 12px 0";
+        a.style.display = "inline-block";
+        a.onmouseenter = function() {
+            a.style.background = "rgba(0,0,0,.05)";
+            a.style.color = "#3c3c3c";
+        };
+        a.onmouseleave = function() {
+            a.style.background = "transparent";
+            a.style.color = "#999";
+        };
+        if (i === 0) {
+            a.innerHTML = "Show all";
+            a.onclick = showAll;
+        } else {
+            a.innerHTML = i;
+            a.onclick = function() {
+                for (let j = 0; j < nodes.length; j++) {
+                    if (nodes[j].innerText == i) show(j);
+                    else hide(j);
+                }
+            };
+        }
+        document.querySelector(".mAsUf").append(a);
+    }
+})();
--- README.md
+++ README.md
@@ -3,3 +3,9 @@
 ### Memrise review numbers
 
 [https://github.com/milankostak/Websites-tweaks/raw/master/Memrise/MemriseReviewNumbers.user.js](https://github.com/milankostak/Websites-tweaks/raw/master/Memrise/MemriseReviewNumbers.user.js)
+
+# Duolingo
+
+### Duolingo Crown Filter
+
+[https://github.com/milankostak/Websites-tweaks/raw/master/Duolingo/DuolingoCrownFilter.user.js](https://github.com/milankostak/Websites-tweaks/raw/master/Duolingo/DuolingoCrownFilter.user.js)
